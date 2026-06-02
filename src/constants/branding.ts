576ddeb4e40a046463883149d40b6e70ef29f3b6
PedroCCallejas <pedroccallejas@hotmail.com>
2026-06-02 00:49:59 -0400

    ajuste

--- src/constants/branding.ts
+++ src/constants/branding.ts
@@ -1,11 +1,11 @@
-export const APP_NAME = 'Professô FC';
-export const APP_SHORT_NAME = 'Professô FC';
+export const APP_NAME = "Professô FC";
+export const APP_SHORT_NAME = "Professô FC";
 export const APP_DESCRIPTION =
-  'Organize seu time, monte escalações, acompanhe estatísticas e marque amistosos.';
-export const APP_TAGLINE = 'Seu futebol organizado.';
-export const APP_THEME_COLOR = '#051108';
-export const APP_BACKGROUND_COLOR = '#051108';
+  "Organize seu time, monte escalações, acompanhe estatísticas e marque amistosos.";
+export const APP_TAGLINE = "Seu futebol organizado.";
+export const APP_THEME_COLOR = "#051108";
+export const APP_BACKGROUND_COLOR = "#051108";
 export const APP_SEO_TITLE = `${APP_NAME} | Futebol amador organizado`;
 export const APP_SEO_DESCRIPTION = APP_DESCRIPTION;
 export const APP_SEO_KEYWORDS =
-  'futebol amador, time de futebol, escalação, estatísticas, amistosos, gestão esportiva';
+  "futebol amador, time de futebol, escalação, estatísticas, amistosos, gestão esportiva, pelada, resenha , organização de futebol, aplicativo de futebol, futebol organizado";
